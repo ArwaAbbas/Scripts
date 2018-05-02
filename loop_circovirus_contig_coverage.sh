deae488eb6d73ee8f3153dac7fc22d3a9cade476
ArwaAbbas <abbas.arwa@gmail.com>
2018-05-02 10:20:38 -0400

    Added another script

--- loop_circovirus_contig_coverage.sh
+++ loop_circovirus_contig_coverage.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-#for file in $(ls *.bam); do
+#for file in $(ls all_viruses*.bam); do
+
 #test for one file
-#for file in $(ls AcinetobacterphageZZ1_D11002*.bam); do
-        #filename=${file%%.bam}
-        #echo $filename
-	#samtools view -b "$filename".bam|genomeCoverageBed -ibam stdin|grep -v 'genome'|/home/cchehoud/scripts/SAMformat2Coverage/coverage_counter.pl > "$filename"_genomecoverage.txt
-	#mv "$filename"_genomecoverage.txt Coverage/
-#done
-
-#Getting genome coverage of bowtie2 alignments of HCRV_[C/D]
-
-for file in $(ls /media/THING2/louis/02_VirusHunting/CircoAlignments/*.bam);do
-	filename=${file%%.bam}
-	samplename=${filename##/media/THING2/louis/02_VirusHunting/CircoAlignments/}
-	echo $filename $samplename
-	samtools view -b "$filename".bam|genomeCoverageBed -ibam stdin|grep -v 'genome'|perl /home/cchehoud/scripts/SAMformat2Coverage/coverage_counter.pl > "$samplename"_genomecoverage.txt
-	mv "$samplename"_genomecoverage.txt Coverage/
+for file in $(ls all_viruses_D13017.bam); do
+        filename=${file%%.bam}
+        echo $filename
+	samtools view -b "$filename".bam|genomeCoverageBed -ibam stdin|grep -v 'genome'| perl /media/THING2/arwa/1_Scripts/coverage_counter.pl > "$filename"_genomecoverage.txt
+	mv "$filename"_genomecoverage.txt Coverage/
 done
